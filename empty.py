407f8dbaf9296929ee69c4aa7d2e2d3435268879
tatiana-zozulina <freya13666@gmail.com>
2013-09-19 23:23:29 +0600

    Subtraction in empty.py

--- empty.py
+++ empty.py
@@ -6,6 +6,16 @@ import sys
 def main():
 	print "Command-line arguments:"
 	print sys.argv
+	x = raw_input("Please, enter first number: ")
+	y = raw_input("Please, enter second number: ")
+	try:
+		x = int(x)
+		y = int(y)
+	except ValueError:
+		print "Unexpected value."
+		return
+	z = x-y
+	print "Subtraction result:", z
 
 if __name__ == "__main__":
 	main()
